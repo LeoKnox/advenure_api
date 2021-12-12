ca89c4bf7fd177037dc25c7bc126c47ad3224398
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-12 11:42:58 -0800

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,1 +1,10 @@
-a
+from django.db import models
+
+from django.db import models
+from django.core.validators import RegexValidator
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=True)
+    length = models.IntegerField(default=5)
+    width = models.IntegerField(default=5)
