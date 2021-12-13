a27b3ded1b4ad450d8c10f142467a16d74aeb03c
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-13 09:53:21 -0800

    Add files via upload

--- front_adventure_api/src/App.js
+++ front_adventure_api/src/App.js
@@ -1,1 +1,19 @@
-a
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Web App for Building Rooms
+        </p>
+        <h2>Adventure API</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
