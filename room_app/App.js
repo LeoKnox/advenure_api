38eb74d07099bd43ed2c4e137497bff2bf03a3ab
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-16 09:32:05 -0800

    Add files via upload

---
+++ room_app/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  const myText = "Go build adventures!"
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>API Adventure Mobile App</Text>
+      <Text style={styles.newText}>{myText}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  baseText: {
+    color: "navy",
+    fontSize: 30,
+    fontStyle: "italic",
+  },
+  newText: {
+    color: "red",
+  }
+});
