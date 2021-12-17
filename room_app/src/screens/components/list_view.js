27468f236533aa0707b4fe94b1b7e064a667b776
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-17 12:33:10 -0800

    Add files via upload

--- room_app/src/screens/components/list_view.js
+++ room_app/src/screens/components/list_view.js
@@ -1,1 +1,27 @@
-a
+import React, { Componenet } from "react";
+import {StyleSheet, View, Text, Button } from "react-native";
+
+class ListView extends Component {
+    render() {
+        return (
+            <View style={styles.center}>
+                <Text style={styles.title}>List of Rooms</Text>
+                <Button title="Room! Click for Details" />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fonstSize: 36,
+        marginBottom: 16,
+    },
+});
+
+export default ListView;
