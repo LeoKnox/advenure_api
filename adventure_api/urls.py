2e7fd34857cbb8c9ca3a099bbad556b350d92b52
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-22 10:29:20 -0800

    Add files via upload

--- adventure_api/urls.py
+++ adventure_api/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rooms.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
