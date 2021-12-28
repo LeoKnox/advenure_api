ed99979dc3815f449869c4e0711ccfa3690f640b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-28 10:53:57 -0800

    Add files via upload

--- room_app/src/screens/drawer/screenA.js
+++ room_app/src/screens/drawer/screenA.js
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {StyleSheet, Image, SafeAreaView, TextInput, Text } from "react-native";
 
 const ScreenA = () => {
+    const [room, setRoom] = useState(" ");
     return (
         <SafeAreaView>
+            <Text
+                style={{
+                    marginTop: 100,
+                    fontSize: 40,
+                    color: "red",
+                    textAlign: "center",
+                }}
+            >
+                {room}
+            </Text>
             <TextInput
                 style={styles.textBox}
                 autoCapitalize="words"
                 autoCorrect={false}
                 placeholder="Character Name"
+                onChangeText={(text) => setRoom(text)}
             />
         </SafeAreaView>
     );
