a15f87949fd32520468fa7391ba3ecdc973a68ae
LeoKnox <LeoKnox@users.noreply.github.com>
2021-12-28 11:50:33 -0800

    Add files via upload

--- room_app/src/screens/drawer/screenA.js
+++ room_app/src/screens/drawer/screenA.js
@@ -1,34 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import {StyleSheet, Image, SafeAreaView, TextInput, Text } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const ScreenA = () => {
-    const [room, setRoom] = useState(" ");
     return (
         <SafeAreaView>
-            <Text
-                style={{
-                    marginTop: 100,
-                    fontSize: 40,
-                    color: "red",
-                    textAlign: "center",
+            <Formik
+                intitialValues={{ room: "", description: "" }}
+                onSubmit={(values) => {
+                    alert(JSON.stringify(values, null, 2));
                 }}
             >
-                {room}
-            </Text>
-            <TextInput
-                style={styles.textBox}
-                autoCapitalize="words"
-                autoCorrect={false}
-                placeholder="Character Name"
-                onChangeText={(text) => setRoom(text)}
-            />
+                {({ handleChange, handleSubmit, values }) => (
+                    <>
+                        <TextInput
+                            syle={styles.textBox}
+                            value={values.room}
+                            type="text"
+                            placeholder="Enter Room Name"
+                            onChangeText={handleChange("room")}
+                        />
+                        <TextInput
+                            style={styles.textBox}
+                            value={values.description}
+                            placeholder="Room Description"
+                            onChangeText={handleChange("description")}
+                        />
+                        <Button onPress={handleSubmit} title="Submit" />
+                    </>
+                )}
+            </Formik>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     textBox: {
-        marginTop: 200,
+        marginTop: 50,
         height: 40,
         borderColor: "gray",
         borderWidth: 1,
