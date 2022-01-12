ccb9cd1136220e642ce49d231ca7e6f1b8f6d343
LeoKnox <LeoKnox@users.noreply.github.com>
2022-01-12 12:11:00 -0800

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.parsers import FormParser, MultiPartParser
-from .serializers import RoomListSerializer, RoomDetailSerializer
-from .models import Room
+from .serializers import KanjiListSerializer
+from .models import Kanji
 
-class RoomListAPIView(generics.ListAPIView):
-    queryset = Room.objects.all()
-    serializer_class = RoomListSerializer
-
-class RoomRetrieveAPIView(generics.RetrieveAPIView):
-    lookup_field = "id"
-    queryset = Room.objects.all()
-    serializer_class = RoomDetailSerializer
-
-class RoomCreateAPIView(generics.CreateAPIView):
-    parser_classes = [MultiPartParser]
-    queryset = Room.objects.all()
-    serializer_class = RoomDetailSerializer
-
-class RoomRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    lookup_field = "id"
-    queryset = Room.objects.all()
-    serializer_class = RoomDetailSerializer
-
-class RoomDestroyAPIView(generics.DestroyAPIView):
-    lookup_field = "id"
-    queryset = Room.objects.all()
+class KanjiListAPIView(generics.ListAPIView):
+    queryset = Kanji.objects.all()
+    serializer_class = KanjiListSerializer
